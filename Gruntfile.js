183ccc60893f5a48d705bb89d788155f0c48de3f
BorekBeker <bekerborek@gmail.com>
2016-04-21 11:24:15 +0200

    Imported grunt into Travis

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,5 +77,6 @@ module.exports = function(grunt) {
 
   // Default task.
   grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
+  grunt.registerTask('travis', ['qunit']);
 
 };
