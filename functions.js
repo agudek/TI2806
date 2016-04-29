cf5f0b9e6e2aee16083a47917cf2fbb6d25bcf49
Marco Boom <m.boom@student.tudelft.nl>
2016-04-29 09:16:31 +0200

    changed url to fetch json from the api

--- functions.js
+++ functions.js
@@ -1,11 +1,11 @@
-function get(url, callback) {
+function get(query, callback) {
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
-            callback(http.responseText);
+            callback(JSON.parse(http.responseText));
         }
     };
-    http.open('GET', url, true);
+    http.open('GET', host + query + '?format=json', true);
     http.setRequestHeader('Access-Control-Allow-Headers', 'x-requested-with');
     http.send(null);
 }
