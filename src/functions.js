c0a24889a625e5e1a3ff741cb166f575d63e0560
Marco Boom <m.boom@student.tudelft.nl>
2016-05-02 12:04:41 +0200

    added http error callback

--- src/functions.js
+++ src/functions.js
@@ -3,6 +3,12 @@ function get(query, callback) {
     http.onreadystatechange = function () {
         if (http.readyState == 4 && http.status == 200) {
             callback(JSON.parse(http.responseText));
+        } else {
+            var error = {
+                'http-status': http.status,
+                'response-headers': http.getAllResponseHeaders().toString()
+            }
+            callback(error);
         }
     };
     http.open('GET', host + query.replace(host, '') + '?format=json', true);
