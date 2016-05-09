021501684ece842a9379c2cfb6af3124e3a34cb0
BorekBeker <bekerborek@gmail.com>
2016-05-09 10:52:59 +0200

    Init will be loaded after jquery has been loaded

---
+++ src/aggregation.js
@@ -0,0 +1,1 @@
+$('#pullrequests-count').text('7');
--- src/init.js
+++ src/init.js
@@ -1,15 +1,19 @@
-var scriptfiles = [
-    'https://code.jquery.com/jquery-2.2.3.js',
-    'settings.js',
-    'functions.js'
-];
+$(document).ready(function () {
+	var scriptfiles = [
+	    'settings.js',
+	    'functions.js',
+	    'dummy.js',
+	    'aggregation.js'
+	];
 
-loadScripts(scriptfiles);
-function loadScripts(scripts) {
-    for (var i = 0; i < scripts.length; ++i) {
-        var script = document.createElement('script');
-        script.src = scripts[i];
-        script.type = 'text/javascript';
-        document.getElementsByTagName('head')[0].appendChild(script);
-    }
-}
+	loadScripts(scriptfiles);
+	function loadScripts(scripts) {
+	    for (var i = 0; i < scripts.length; ++i) {
+		var script = document.createElement('script');
+		script.id = "script-" + scripts[i];
+		script.src = scripts[i];
+		script.type = 'text/javascript';
+		$('head')[0].appendChild(script);
+	    }
+	}
+});
