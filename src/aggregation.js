0783cabcab648886d30b349eb0e55617b7ce47dd
BorekBeker <bekerborek@gmail.com>
2016-05-10 11:03:20 +0200

    Made a service object for all octopeer calls

--- src/aggregation.js
+++ src/aggregation.js
@@ -1,1 +1,23 @@
-$('#pullrequests-count').text('7');
+function OctopeerService() {
+	var settings = new Settings();
+	//var caller = new OctopeerCaller(settings.host);
+	var caller = new DummyCaller(settings.host);
+
+	this.getPullRequestsAmount = function(callback){
+		caller.get('/pull-requests/', function (pullrequests) {
+			callback(pullrequests.count)
+		});
+	}
+
+	this.getUserAmount = function(callback){
+		caller.get('/users/', function (users) {
+			callback(users.count);
+		});
+	}
+}
+
+var octopeerService = new OctopeerService();
+octopeerService.getPullRequestsAmount(function (amount) {
+	$('#pullrequests-count').text(amount);
+});
+
--- src/functions.js
+++ src/functions.js
@@ -1,16 +1,19 @@
-function get(query, callback) {
-    var http = new XMLHttpRequest();
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            callback(JSON.parse(http.responseText));
-        } else {
-            var error = {
-                'http-status': http.status,
-                'response-headers': http.getAllResponseHeaders().toString()
-            }
-            callback(error);
-        }
-    };
-    http.open('GET', host + query.replace(host, '') + '?format=json', true);
-    http.send(null);
+function OctopeerCaller(host) {
+	this.get = function get(query, callback) {
+	    var http = new XMLHttpRequest();
+	    http.onreadystatechange = function () {
+		if (http.readyState == 4 && http.status == 200) {
+		    callback(JSON.parse(http.responseText));
+		} else {
+		    var error = {
+			'http-status': http.status,
+			'response-headers': http.getAllResponseHeaders().toString()
+		    }
+		    callback(error);
+		}
+	    };
+	    http.open('GET', host + query.replace(host, '') + '?format=json', true);
+	    http.send(null);
+	 }
 }
+
