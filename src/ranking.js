e11169bfb4f4b9f3a76138f8408335826e124d17
Marco Boom <m.boom@student.tudelft.nl>
2016-05-13 10:53:59 +0200

    created datastructure for DR

--- src/ranking.js
+++ src/ranking.js
@@ -1,26 +1,8 @@
 var Rank = function () {
     var RE = new REmap();
     var FR = new FRmap();
+    var DR = new DRmatrix();
     var RF = new RFmap();
-    var Dr = {
-        "ranking_js": ["marco", "leendert", "borek"],
-        "testing_js": []
-    }
-    function Dr(f) {
-        return Dr.f;
-    }
-    function Dru() {
-        var reviewers = [];
-        for (var i = 0; i < Object.keys(Dr).length; ++i) {
-            for (var j = 0; j < Dr[i].length; ++j) {
-                var reviewer = Dr[i][j];
-                if (typeof reviewers[reviewer] == 'undefined') {
-                    reviewers.push(reviewer);
-                }
-            }
-        }
-        return reviewers;
-    }
     
     function xFactor(r, f) {
         var reviewer = RE.RE(r, f);
@@ -32,7 +14,6 @@ var Rank = function () {
             val += 1 / (reviewer.f.T - file.T);
         }
     }
-
     function Score(r) {
         var val = 0;
         for (var i = 0; i < RE.getFiles(r) ; ++i) {
@@ -40,80 +21,133 @@ var Rank = function () {
             val += xFactor(r, f);
         }
     }
-};
+    function Scores() {
+        var dru = DR.dru();
+        for (var i = 0; i < dru.length; ++i) {
+            var r = dru[i];
+            RF.addScore(r, Score(r));
+        }
+    }
 
-var REmap = function () {
-    var map = {
-        'marco': {
+    this.rank = function (m) {
+        return RF.RF(m);
+    };
+
+    DR.add(RE);
+    Scores();
+
+    var REmap = function () {
+        var map = {
+            'marco': {
+                'ranking_js': {
+                    'C': 0,
+                    'W': 0,
+                    'T': 0
+                }
+            }
+        };
+
+        this.RE = function (r, f) {
+            f = f.replace('.', '_');
+            return map.r.f;
+        };
+
+        this.addReviewer = function (r, f) {
+            map.r = f;
+        };
+        this.addFile = function (r, f, C, W, T) {
+            f = f.replace('.', '_');
+            map.r = {};
+            map.r.f = {
+                'C': C,
+                'W': W,
+                'T': T
+            };
+        };
+
+        this.getReviewers = function () {
+            return Object.keys(map);
+        }
+        this.getFiles = function (r) {
+            return Object.keys(map.r);
+        }
+    };
+
+    var FRmap = function () {
+        var map = {
             'ranking_js': {
                 'C': 0,
                 'W': 0,
                 'T': 0
             }
-        }
-    };
+        };
 
-    this.RE = function (r, f) {
-        f = f.replace('.', '_');
-        return map.r.f;
-    };
-
-    this.addReviewer = function (r, f) {
-        map.r = f;
-    };
-    this.addFile = function (r, f, C, W, T) {
-        f = f.replace('.', '_');
-        map.r = {};
-        map.r.f = {
-            'C': C,
-            'W': W,
-            'T': T
+        this.FR = function (f) {
+            f = f.replace('.', '_');
+            return map.f;
+        };
+        this.addFile = function (f, C, W, T) {
+            map.f = {
+                'C': C,
+                'W': W,
+                'T': T
+            };
         };
     };
 
-    this.getFiles = function (r) {
-        return Object.keys(map.r);
-    }
-};
+    var DRmatrix = function () {
+        var map = {
+            'ranking_js': ['marco', 'leendert', 'borek'],
+            'testing_js': []
+        };
 
-var FRmap = function () {
-    var map = {
-        'ranking_js': {
-            'C': 0,
-            'W': 0,
-            'T': 0
-        }
-    };
-
-    this.FR = function (f) {
-        f = f.replace('.', '_');
-        return map.f;
-    };
-    this.addFile = function (f, C, W, T) {
-        map.f = {
-            'C': C,
-            'W': W,
-            'T': T
+        this.add = function (RE) {
+            for (var i = 0; i < RE.getReviewers().length; ++i) {
+                var r = RE.getReviewers()[i];
+                var files = RE.getFiles(r);
+                for (var j = 0; j < files.length; ++j) {
+                    var f = RE.getFiles(r)[j];
+                    if (typeof map.f == 'undefined') {
+                        map.f = [];
+                    }
+                    map.f.push(r);
+                }
+            }
+        };
+        this.dr = function (f) {
+            return map.f;
+        };
+        this.dru = function () {
+            var reviewers = [];
+            for (var i = 0; i < Object.keys(map).length; ++i) {
+                for (var j = 0; j < map[i].length; ++j) {
+                    var reviewer = map[i][j];
+                    if (typeof reviewers[reviewer] == 'undefined') {
+                        reviewers.push(reviewer);
+                    }
+                }
+            }
+            return reviewers;
         };
     };
-};
 
-var RFmap = function () {
-    var map = {
-        'marco': 0
-    };
+    var RFmap = function () {
+        var map = {
+            'marco': 0
+        };
     
-    this.addScore = function (r, s) {
-        map.r = s;
+        this.addScore = function (r, s) {
+            map.r = s;
+        };
+        this.RF = function (m) {
+            var arr = new Array();
+            for (var i = 0; i < Object.keys(map).length; ++i) {
+                var x = new Array();
+                x.push(Object.keys(map)[i]);
+                x.push(map[i]);
+            }
+            arr.sort(function (a, b) { return (b[1] - a[1]); });
+            return arr.slice(0, m);
+        };
     };
-    this.RF = function (m) {
-        var arr = new Array();
-        for (var i = 0; i < Object.keys(map).length; ++i) {
-            var x = new Array();
-            x.push(Object.keys(map)[i]);
-            x.push(map[i]);
-        }
-        arr.sort(function (a, b) { return (b[1] - a[1]); });
-        return arr.slice(0, m);
-    }
-}
+};
