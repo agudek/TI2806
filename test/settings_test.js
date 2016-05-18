b1ab77147f898239fe46ca0e809e1f56109d314c
Daan van der Valk <daanvdvalk@gmail.com>
2016-05-18 10:24:30 +0200

    Settings test changed to Qunit conventions

--- test/settings_test.js
+++ test/settings_test.js
@@ -1,6 +1,15 @@
-QUnit.test("Check the settings hostname for a valid domain or IP address", function(assert){
-    var settings = new Settings();
-    var matchesIP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-    var matchesDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
-    assert.ok(settings.hostname.match(matchesIP) || settings.hostname.match(matchesDomain) );
-})
+(function($) {
+    module('Settings for the connection to the Octopeer server', {
+        // Set up this module
+        setup: function() {
+          this.settings = new Settings();
+        }
+    });
+
+
+    test("Check the settings hostname for a valid domain or IP address", function(assert){
+        var matchesIP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+        var matchesDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
+        ok((this.settings.hostname.match(matchesIP) || this.settings.hostname.match(matchesDomain)), 'is valid');
+    })
+}(jQuery));
