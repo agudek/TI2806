a2fbc496b6b97f74507b6702c124bdadd9c2ae6e
Daan van der Valk <daanvdvalk@gmail.com>
2016-05-18 10:20:48 +0200

    Undo previous commit 0b6af6cb3bf3e1b03e8185a937721692ee4972ac Resetting the Octopeer connection settings

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 function Settings() {
 	this.protocol = 'http';
-	this.hostname = '1988';
+	this.hostname = '146.185.128.124';
 	this.port = '';
 	this.host = this.protocol + '://' + this.hostname + ((this.port.length > 0) ? ':' + this.port : '') + '/api';
 }
