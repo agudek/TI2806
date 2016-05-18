73bfda25dd1a6cf345c75bc09c2cbee1d32bbcf7
Daan van der Valk <daanvdvalk@gmail.com>
2016-05-18 09:52:16 +0200

    Added mini unit test for the settings file

---
+++ test/settings_test.js
@@ -0,0 +1,6 @@
+QUnit.test("Check the settings hostname for a valid domain or IP address", function(assert){
+    var settings = new Settings();
+    var matchesIP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    var matchesDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
+    assert.ok(settings.hostname.match(matchesIP) || settings.hostname.match(matchesDomain) );
+})
