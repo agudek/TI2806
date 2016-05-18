2f3597c90cf1e3c286232a98508c6741f78d767a
Daan van der Valk <daanvdvalk@gmail.com>
2016-05-18 11:45:34 +0200

    Automatically test all non-visualisation modules Use RequireJS to load all modules and their test counterparts

--- test/settings_test.js
+++ test/settings_test.js
@@ -1,4 +1,4 @@
-(function($) {
+(function() {
     module('Settings for the connection to the Octopeer server', {
         // Set up this module
         setup: function() {
@@ -7,9 +7,9 @@
     });
 
 
-    test("Check the settings hostname for a valid domain or IP address", function(assert){
+    test("Hostname should be domain or IP address", function(assert){
         var matchesIP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
         var matchesDomain = /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/;
         ok((this.settings.hostname.match(matchesIP) || this.settings.hostname.match(matchesDomain)), 'is valid');
     })
-}(jQuery));
+}());
