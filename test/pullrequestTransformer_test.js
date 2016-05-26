a5ab3445a45d39a07d54b368f789957598a00a2d
Leendert van Doorn <l.b.vandoorn@student.tudelft.nl>
2016-05-26 10:56:07 +0200

    Add empty test files

---
+++ test/pullrequestTransformer_test.js
@@ -0,0 +1,15 @@
+(function () {
+    module('Create a pullrequestTransformer object', {
+        // Set up this module
+        setup: function () {
+            this.prt = new PullRequestTransformer();
+        }
+    });
+
+
+    test("Transform a GitHub pullrequest", function (assert) {
+
+        
+        
+    })
+}());
