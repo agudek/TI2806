77c703f1a56b36fd27a27f5afd874dd38d2e757c
Marco Boom <m.boom@student.tudelft.nl>
2016-05-31 10:43:59 +0200

    reformatted graph1

--- src/modules/graph1.js
+++ src/modules/graph1.js
@@ -1,5 +1,47 @@
 /* globals define */
 define(function () {
+    var w = 720,
+    h = 350,
+    pad = 50,
+    padTop = 10,
+    padBottom = 50
+    sizeData = [
+                { 'x': 1, 'y': 6 },
+                { 'x': 2, 'y': 5 },
+                { 'x': 4, 'y': 5 }
+    ],
+    domain = ['0-2', '3-5', '5-10', '<10'],
+    buckets = [];
+    for (var i = 0; i < domain.length; ++i) {
+        buckets.push(0);
+    }
+
+    function devide(x) {
+        switch (true) {
+            case (x > 10): return 3; // > 10
+            case (x >= 6 && x <= 10): return 2; // 6 - 10
+            case (x >= 3 && x <= 5): return 1; // 3 - 5
+            case (x >= 0 && x <= 2): return 0; //0 - 2
+            default: return 0;
+        }
+    }
+    for (i = 0; i < sizeData.length; ++i) {
+        var item = sizeData[i];
+        buckets[devide(item.x)] += item.y;
+    }
+    sizeData = [];
+    for (i = 0; i < buckets.length; ++i) {
+        sizeData.push({ 'x': i, 'y': buckets[i] });
+    }
+
+    var maxValue = Math.max.apply(Math, sizeData.map(function (o) { return o.y; })),
+    xSizeScale = d3.scale.linear()
+        .domain([0, domain.length])
+        .range([pad, w - 30]),
+    ySizeScale = d3.scale.linear()
+        .domain([0, maxValue])
+        .range([padTop, h - padBottom]).nice();
+
     return {
         name: 'graph1',
         title: 'Number of pull-request per number of comments',
@@ -15,95 +57,19 @@ define(function () {
         xAxisLabelRotation: 65,
         yAxisScale: function() { return "fit"; },
         xAxisScale: function() { 
-            var domain = ['0-2', '3-5', '5-10', '<10'],
-            scale = d3.scale.ordinal()
+            return d3.scale.ordinal()
                 .domain(domain)
                 .rangePoints(
                     [0.5 * (720 / domain.length - 40), 720 - 2 * 50 - 0.5 * (720 / domain.length - 40)]
                 );
-            return scale;
         },
-        yAxisFitFunction: function() { 
-            var sizeData = [
-                { 'x': 1, 'y': 6 },
-                { 'x': 2, 'y': 5 },
-                { 'x': 4, 'y': 5 }
-            ],
-            domain = ['0-2', '3-5', '5-10', '<10'],
-            buckets = [];
-            for (var i = 0; i < domain.length; ++i) {
-                buckets.push(0);
-            }
-
-            function devide(x) {
-                switch (true) {
-                    case (x > 10): return 3; // > 10
-                    case (x >= 6 && x <= 10): return 2; // 6 - 10
-                    case (x >= 3 && x <= 5): return 1; // 3 - 5
-                    case (x >= 0 && x <= 2): return 0; //0 - 2
-                    default: return 0;
-                }
-            }
-            for (i = 0; i < sizeData.length; ++i) {
-                var item = sizeData[i];
-                buckets[devide(item.x)] += item.y;
-            }
-            sizeData = [];
-            for (i = 0; i < buckets.length; ++i) {
-                sizeData.push({ 'x': i, 'y': buckets[i] });
-            }
-
-            var maxValue = Math.max.apply(Math, sizeData.map(function (o) { return o.y; }));
-
+        yAxisFitFunction: function() {
             return [0, maxValue];            
          },
         xAxis: true,
         yAxis: true,
         yRightAxis: false,
         body: function () {
-            var w = 720,
-                h = 350,
-                pad = 50,
-                padTop = 10,
-                padBottom = 50,
-                sizeData = [
-                    { 'x': 1, 'y': 6 },
-                    { 'x': 2, 'y': 5 },
-                    { 'x': 4, 'y': 5 }
-                ],
-                domain = ['0-2', '3-5', '5-10', '<10'],
-                buckets = [];
-            for (var i = 0; i < domain.length; ++i) {
-                buckets.push(0);
-            }
-
-            function devide(x) {
-                switch (true) {
-                    case (x > 10): return 3; // > 10
-                    case (x >= 6 && x <= 10): return 2; // 6 - 10
-                    case (x >= 3 && x <= 5): return 1; // 3 - 5
-                    case (x >= 0 && x <= 2): return 0; //0 - 2
-                    default: return 0;
-                }
-            }
-            for (i = 0; i < sizeData.length; ++i) {
-                var item = sizeData[i];
-                buckets[devide(item.x)] += item.y;
-            }
-            sizeData = [];
-            for (i = 0; i < buckets.length; ++i) {
-                sizeData.push({ 'x': i, 'y': buckets[i] });
-            }
-
-            var maxValue = Math.max.apply(Math, sizeData.map(function (o) { return o.y; }));
-
-            var xSizeScale = d3.scale.linear()
-                    .domain([0, domain.length])
-                    .range([pad, w - 30]),
-            ySizeScale = d3.scale.linear()
-                    .domain([0, maxValue])
-                    .range([padTop, h - padBottom]).nice();
-
             var g = d3.select(document.createElementNS(d3.ns.prefix.svg, "g"));
 
             g.selectAll("rect").data(sizeData).enter()
