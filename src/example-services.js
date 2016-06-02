9c8556b170e2b86ac3a6e9b1b1f9d86d51f7d5ab
BorekBeker <bekerborek@gmail.com>
2016-06-02 11:33:49 +0200

    Removed example-service

--- src/example-services.js
+++ src/example-services.js
@@ -1,8 +1,6 @@
-/*globals Graph2Aggregator, console*/
-
-new Graph2Aggregator("mboom", 5).then(function (result) {
-    console.log(result);
-});
+//new Graph2Aggregator("mboom", 5).then(function (result) {
+//    //console.log(result);
+//});
 
 //var githubService = new GithubService();
 //githubService.getPullRequests('mboom', 'TI2806', function (pullrequest) {
